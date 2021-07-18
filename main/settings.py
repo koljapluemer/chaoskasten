9a49b6e8032f611063603c8be633234bc638387d
blackbrokkoli <kastomat@gmail.com>
2021-07-18 10:12:40 +0200

    change env type and some settings

--- main/settings.py
+++ main/settings.py
@@ -1,29 +1,9 @@
 import django_heroku
 import os
-import environ
-
-env = environ.Env(
-    # set casting, default value
-    DEBUG=(bool, False),
-    STRIPE_LIVE_MODE=(bool, False)
-)
-environ.Env.read_env()
 
 USE_TZ = True
-
-# Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*pn5tp97vgmt@k(+ao87n9%q80&jyk7@jefjewfewpofodo8yxgtg4g0jk6'
-
-
 ALLOWED_HOSTS = ['chaoskasten.com', 'localhost']
-
 
 # Application definition
 
@@ -80,10 +60,6 @@ DATABASES = {
     }
 }
 
-
-# Password validation
-# https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
-
 AUTH_PASSWORD_VALIDATORS = [
     {
         'NAME': 'django.contrib.auth.password_validation.UserAttributeSimilarityValidator',
@@ -98,7 +74,6 @@ AUTH_PASSWORD_VALIDATORS = [
         'NAME': 'django.contrib.auth.password_validation.NumericPasswordValidator',
     },
 ]
-
 
 # Internationalization
 # https://docs.djangoproject.com/en/3.0/topics/i18n/
@@ -120,34 +95,34 @@ LOGIN_REDIRECT_URL = '/notes'
 LOGOUT_REDIRECT_URL = '/'
 
 
-DOMAIN_URL = env('DOMAIN_URL')
+DOMAIN_URL = os.getenv('DOMAIN_URL', "https://chaoskasten.herokuapp.com")
 
 if env.get_value("STRIPE_LIVE_MODE"):
-    STRIPE_PUBLIC_KEY = env('STRIPE_LIVE_PUBLIC_KEY')
-    STRIPE_SECRET_KEY = env('STRIPE_LIVE_SECRET_KEY')
+    STRIPE_PUBLIC_KEY = os.getenv('STRIPE_LIVE_PUBLIC_KEY')
+    STRIPE_SECRET_KEY = os.getenv('STRIPE_LIVE_SECRET_KEY')
 else:
-    STRIPE_PUBLIC_KEY = env("STRIPE_TEST_PUBLIC_KEY")
-    STRIPE_SECRET_KEY = env("STRIPE_TEST_SECRET_KEY")
+    STRIPE_PUBLIC_KEY = os.getenv("STRIPE_TEST_PUBLIC_KEY")
+    STRIPE_SECRET_KEY = os.getenv("STRIPE_TEST_SECRET_KEY")
 
 
-STRIPE_PRICE_ID = env("STRIPE_PRICE_ID")
+STRIPE_PRICE_ID = os.getenv("STRIPE_PRICE_ID")
 
 APPEND_SLASH=False
 
-SECRET_KEY = env('SECRET_KEY')
+SECRET_KEY = os.getenv('SECRET_KEY', "*pn5tp97vgmt@k(+ao87n9%q80&jyk7@jefjewfewpofodo8yxgtg4g0jk6")
 
 # false when it does not exist (wtf)
-DEBUG = env('DEBUG')
+DEBUG = os.getenv('DEBUG')
 
 
-SENDGRID_API_KEY = env('SENDGRID_API_KEY')
+SENDGRID_API_KEY = os.getenv('SENDGRID_API_KEY')
 
 EMAIL_HOST = 'smtp.sendgrid.net'
 EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
 EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
 EMAIL_PORT = 587
 EMAIL_USE_TLS = True
-DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+DEFAULT_FROM_EMAIL = os.getenv('DEFAULT_FROM_EMAIL')
 
 # Activate Django-Heroku.
 django_heroku.settings(locals())
