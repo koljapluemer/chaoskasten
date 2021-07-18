808ecf79b802d5f280ad8d3a2ae0d2816bb95843
blackbrokkoli <kastomat@gmail.com>
2021-07-18 09:59:03 +0200

    attempt heroku push with inspirationbot settings

--- main/settings_old.py
+++ main/settings_old.py
@@ -1,36 +1,38 @@
-import django_heroku
 import os
+import environ
 
-USE_TZ = True
+import django_heroku
+
+env = environ.Env(
+    # set casting, default value
+    DEBUG=(bool, False),
+    STRIPE_LIVE_MODE=(bool, False)
+)
+environ.Env.read_env()
+
+ALLOWED_HOSTS = ['chaoskasten.com', 'localhost']
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# Quick-start development settings - unsuitable for production
-# See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
-
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*pn5tp97vgmt@k(+ao87n9%q80&jyk7@j=9odo8yxgtg4g0jk6'
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
-
-ALLOWED_HOSTS = []
-
-
 # Application definition
-
 INSTALLED_APPS = [
+    'main',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'webapp',
-    'django_extensions',
+    'lazysignup',
+    'stripe',
 ]
+
+AUTHENTICATION_BACKENDS = (
+  'django.contrib.auth.backends.ModelBackend',
+  'lazysignup.backends.LazySignupBackend',
+)
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
@@ -42,7 +44,7 @@ MIDDLEWARE = [
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
 ]
 
-ROOT_URLCONF = 'webapp.urls'
+ROOT_URLCONF = 'main.urls'
 
 TEMPLATES = [
     {
@@ -60,18 +62,7 @@ TEMPLATES = [
     },
 ]
 
-WSGI_APPLICATION = 'webapp.wsgi.application'
-
-
-# Database
-# https://docs.djangoproject.com/en/3.0/ref/settings/#databases
-
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'inspirationDB.sqlite3'),
-    }
-}
+WSGI_APPLICATION = 'main.wsgi.application'
 
 
 # Password validation
@@ -106,10 +97,55 @@ USE_L10N = True
 
 USE_TZ = True
 
-SITE_ROOT = os.path.dirname(os.path.realpath(__file__))
-STATICFILES_DIRS = (
-  os.path.join(SITE_ROOT, 'static/'),
-)
+
+# Static files (CSS, JavaScript, Images)
+# https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+STATIC_ROOT = os.path.join(BASE_DIR, 'static')
+
+LOGIN_REDIRECT_URL = '/notes'
+LOGOUT_REDIRECT_URL = '/'
+
+DOMAIN_URL = env('DOMAIN_URL')
+
+if env.get_value("STRIPE_LIVE_MODE"):
+    STRIPE_PUBLIC_KEY = env('STRIPE_LIVE_PUBLIC_KEY')
+    STRIPE_SECRET_KEY = env('STRIPE_LIVE_SECRET_KEY')
+else:
+    STRIPE_PUBLIC_KEY = env("STRIPE_TEST_PUBLIC_KEY")
+    STRIPE_SECRET_KEY = env("STRIPE_TEST_SECRET_KEY")
+
+
+STRIPE_PRICE_ID = env("STRIPE_PRICE_ID")
+
+APPEND_SLASH=False
+
+SECRET_KEY = env('SECRET_KEY')
+
+# false when it does not exist (wtf)
+DEBUG = env('DEBUG')
+
+DATABASES = {
+    'default': {
+    # 'django.db.backends.postgresql_psycopg2' for production
+        'ENGINE': env('ENGINE'),
+        'NAME': env('DB_NAME'),
+        'USER': env('DB_USER'),
+        'PASSWORD': env('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': '',
+    }
+}
+
+SENDGRID_API_KEY = env('SENDGRID_API_KEY')
+
+EMAIL_HOST = 'smtp.sendgrid.net'
+EMAIL_HOST_USER = 'apikey' # this is exactly the value 'apikey'
+EMAIL_HOST_PASSWORD = SENDGRID_API_KEY
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
+DEFAULT_FROM_EMAIL = env('DEFAULT_FROM_EMAIL')
+
+# Activate Django-Heroku.
 django_heroku.settings(locals())
